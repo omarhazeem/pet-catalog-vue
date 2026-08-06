d4178a95c0097db795e96506963126abeab70542
omarhazeem <omat.hazeem2005@gmail.com>
2026-08-06 15:32:23 +0300

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: 'light',
+  },
+})
+
+createApp(App).use(vuetify).mount('#app')
